b563692fd1f5432ebf4dc403548fec2627a8e649
cyberkitaec <addkudi@mail.ru>
2023-09-18 01:00:31 +0300

    create SingerAdmin, AlbumAdmin, SongAdmin

--- kortex/films/admin.py
+++ kortex/films/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+# Регистрируем модели в админке Django
+
+class SingerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'singer_nickname']
+    list_display_links = ['id', 'singer_nickname']
+    search_fields = ['id', 'singer_nickname']
+
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name_album', 'year_of_release', 'singer_nickname']
+    list_display_links = ['id', 'name_album', 'year_of_release', 'singer_nickname']
+    search_fields = ['id', 'name_album', 'year_of_release', 'singer_nickname']
+
+
+class SongAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name_song', 'number_in_album', 'album']
+    list_display_links = ['id', 'name_song', 'number_in_album', 'album']
+    search_fields = ['id', 'name_song', 'number_in_album', 'album']
+
+
+admin.site.register(Singer, SingerAdmin)
+admin.site.register(Album, AlbumAdmin)
+admin.site.register(Song, SongAdmin)
--- kortex/films/models.py
+++ kortex/films/models.py
@@ -11,6 +11,16 @@ class Singer(models.Model):
     """
     singer_nickname = models.CharField(u'Псевдоним исполнителя', max_length=100)
 
+    # Устанавливаем наименование таблицы, которое будет отображаться в админку - verbose_name
+    # verbose_name_plural - тоже самое что и verbose_name но для множественного числа
+    class Meta:
+        verbose_name = 'Исполнитель'
+        verbose_name_plural = 'Исполнитель'
+
+    # Для корректного отображения объекта на админке Django
+    def __str__(self):
+        return self.singer_nickname
+
 
 class Album(models.Model):
     """
@@ -18,7 +28,14 @@ class Album(models.Model):
     """
     name_album = models.CharField(u'Наименование альбома', max_length=300)
     year_of_release = models.IntegerField(u'Год релиза',choices=YEAR_CHOICES)
-    singer_nickname = models.ForeignKey(Singer, on_delete=models.CASCADE)
+    singer_nickname = models.ForeignKey(Singer, on_delete=models.CASCADE, verbose_name='Псевдоним исполнителя')
+
+    class Meta:
+        verbose_name = 'Альбом'
+        verbose_name_plural = 'Альбом'
+
+    def __str__(self):
+        return self.name_album
 
 
 class Song(models.Model):
@@ -27,5 +44,13 @@ class Song(models.Model):
     """
     name_song = models.CharField(u'Наименование песни', max_length=200)
     number_in_album = models.IntegerField(u'Порядковый номер в альбоме')
-    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, verbose_name='Наименование альбома')
+
+    class Meta:
+        verbose_name = 'Песня'
+        verbose_name_plural = 'Песня'
+
+    def __str__(self):
+        return self.name_song
+
 
